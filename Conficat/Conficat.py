6259f3345ecac4bbea40df05664463cd906ec463
Lorenz Schori <lo@znerol.ch>
2009-09-17 17:04:23 +0200

    [PATCH] refactor command line parser into CLI and main into Conficat modules

---
+++ Conficat/Conficat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""
+Conficat main class
+"""
+__author__ =  'Lorenz Schori'
+__version__=  '0.1'
+
+class ConfigurationError(StandardError):
+  pass
+
+class Conficat(object):
+  def __init__(*args,**kwds):
+    pass
+
+  def run(self):
+    pass
