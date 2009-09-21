2616074254d297f58bb1276fe4dcd603b0ee8697
Lorenz Schori <lo@znerol.ch>
2009-09-21 08:41:46 +0200

    tests for collectfiles

--- test/utilTest.py
+++ test/utilTest.py
@@ -7,8 +7,8 @@ __version__=  '0.1'
 
 import unittest
 import os
-from Conficat.Util import parseCSVFiles
-from tempfile import mkstemp
+from Conficat.Util import parseCSVFiles, collectfiles
+from tempfile import mkstemp, mkdtemp
 
 class ParseCSVFilesTestCase(unittest.TestCase):
   """ Test cases for parseCSVFile utility function"""
@@ -76,6 +76,84 @@ class ParseCSVFilesTestCase(unittest.TestCase):
     for (key,value) in fkey2contents.iteritems():
       self.assertTrue(ParseCSVFilesTestCase.csvdict.has_key(key))
       self.assertEqual(ParseCSVFilesTestCase.csvdict[key],value)
+
+class CollectfilesTestCase(unittest.TestCase):
+  """
+  Testcase for the collectfiles utility function 
+  """
+  def setUp(self):
+    """create temporary directory and files"""
+    self.empty_tmpdir = mkdtemp()
+    self.populated_tmpdir = mkdtemp()
+    (ignored_fd, self.txt_tmpfile) = mkstemp(suffix=".txt",
+                                             dir=self.populated_tmpdir)
+
+  def tearDown(self):
+    """cleanup temporary files and directories"""
+    os.unlink(self.txt_tmpfile)
+    os.removedirs(self.populated_tmpdir)
+    os.removedirs(self.empty_tmpdir)
+  
+  def testCollectfilesExistingSinglefile(self):
+    """collectfiles must return exactly one entry for a single file"""
+    txt_tmpfile_iter = collectfiles(self.txt_tmpfile)
+    self.assertEqual(self.txt_tmpfile, txt_tmpfile_iter.next())
+    self.assertRaises(StopIteration, txt_tmpfile_iter.next)
+
+  def testCollectfilesExistingSinglefileCorrectExtension(self):
+    """
+    collectfiles must return exactly one entry for a single file if the
+    extension parameter contains the txt extension.
+    """
+    txt_tmpfile_iter = collectfiles(self.txt_tmpfile,extensions=["txt","bla"])
+    self.assertEqual(self.txt_tmpfile, txt_tmpfile_iter.next())
+    self.assertRaises(StopIteration, txt_tmpfile_iter.next)
+
+  def testCollectfilesExistingSinglefileWrongExtension(self):
+    """
+    collectfiles must not report any entry if the extension parameter does
+    not contain the correct extension of the testfile
+    """
+    txt_tmpfile_iter = collectfiles(self.txt_tmpfile,extensions=["zz","bla"])
+    self.assertRaises(StopIteration, txt_tmpfile_iter.next)
+
+  def testCollectfilesEmptyDirectory(self):
+    """collectfiles must not return any entry for empty directory"""
+    for x in collectfiles(self.empty_tmpdir):
+      self.fail()
+
+  def testCollectfilesPopulatedDirectory(self):
+    """collectfiles must report one entry for the populated test directory"""
+    txt_tmpfile_iter = collectfiles(self.populated_tmpdir)
+    self.assertEqual(self.txt_tmpfile, txt_tmpfile_iter.next())
+    self.assertRaises(StopIteration, txt_tmpfile_iter.next)
+
+  def testCollectfilesPopulatedDirectoryCorrectExtension(self):
+    """
+    collectfiles must report one entry for the populated test directory if
+    the extension parameter contains the txt extension.
+    """
+    txt_tmpfile_iter = collectfiles(self.populated_tmpdir,
+        extensions=["txt","blabla","test"])
+    self.assertEqual(self.txt_tmpfile, txt_tmpfile_iter.next())
+    self.assertRaises(StopIteration, txt_tmpfile_iter.next)
+
+  def testCollectfilesPopulatedDirectoryWrongExtension(self):
+    """
+    collectfiles must not report any entry for the populated test directory if
+    the extension parameter does not contain the correct extension of the
+    testfile
+    """
+    txt_tmpfile_iter = collectfiles(self.populated_tmpdir,
+        extensions=["xy","blabla","test"])
+    self.assertRaises(StopIteration, txt_tmpfile_iter.next)
+
+  def testCollectfilesNonExistantPath(self):
+    """
+    collectfiles must report an OSError for non-existant paths
+    """
+    txt_tmpfile_iter = collectfiles(os.path.join(self.empty_tmpdir,"nothing"))
+    self.assertRaises(OSError, txt_tmpfile_iter.next)
 
 if __name__ == '__main__':
   unittest.main()
