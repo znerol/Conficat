9ba05fba2b6b95bd3052c7cbcc9fd69bd3380ac6
Lorenz Schori <support@woz.ch>
2009-09-21 17:00:21 +0200

    TemplateRegistry WIP

--- Conficat/TemplateRegistry.py
+++ Conficat/TemplateRegistry.py
@@ -13,8 +13,13 @@ class TemplateRegistry(object):
   def __getitem__(self):
     pass
 
-  def addPath(self, path, data, strip=0, prefix=[]):
+  def addPath(self, path, strip=0, prefix=[]):
     tmplext=(lambda p: os.path.basename(p).split(".")[-1].lower() in ("tmpl","py"))
     for f in findfiles(path,tmplext):
       components=prefix+pathexplode(f)[strip:-1]
       key=str.join(".",components)
+      if self.templates.has_key(key):
+        raise ConfigError("%s: duplicate template entry for key %s" % (path, key))
+      # if extension py ... load py
+      # else ... load tmpl
+
